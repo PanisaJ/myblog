00b29524f4cde1f97a710a6c8c17791d56627d96
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2020-02-14 11:11:54 +0700

    Start project and created Blog, Comment models.

---
+++ myapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=100)
+    blog_text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    user = models.CharField(max_length=100)
+    def __str__(self):
+        return self.blog_title
+
+class Comment(models.Model):
+    comment_text = models.TextField()
+    score = models.IntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    user = models.CharField(max_length=100) 
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.comment_text
+  
